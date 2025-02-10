dfac5972a08d31bb532caabe7df430da58c163b0
Evrat <Evratal@yandex.ru>
2025-02-10 20:55:54 +0300

    Добавлены настройки media

--- config/settings.py
+++ config/settings.py
@@ -117,4 +117,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
